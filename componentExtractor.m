44453a6d3973d712d513174509d183711db13144
Mathews <mathewsTHI@Mathewss-MacBook-Pro.local>
2017-06-14 15:41:36 -0500

    Fixed error in HR variability simulation. Extracted more datasets for Template.

--- componentExtractor.m
+++ componentExtractor.m
@@ -4,35 +4,37 @@
 %
 clear all; 
 %close all; 
-fileNumber = 1;
+fileNumber = 3;
 parentFile = ['parentFile' num2str(fileNumber) '.txt'];
 parentData= readAndParseParentFile(['./Templates/ParentFiles/' parentFile]);
-
+currentDirLength = 30;%length(dir('./Templates/VChannel/AWave/'))-2;
+startChannel = 17;
 %% Extract Data from first 5 channels
-for channel=1:1:4
+for channel=startChannel:1:19
 extractedChannel = parentData{channel}.*5/2^16; %16bit ADC with a max of 5mV
 %figure, plot(extractedChannel);
-HR = 585; %Heart Rate - Calculated from V-V
-EGMBound = 660;
+HR = ceil(60e3/97); %Heart Rate in ms - Calculated from V-V
+EGMBound = 2400;
 EGM = extractedChannel(1:EGMBound); %The first 660 samples are assumed to have a single beat
-figure,plot(EGM);
+ %figure(1),subplot(5,4,channel),plot(EGM), title(num2str(channel)); 
+ %figure(2), hold on, plot(EGM), hold off; 
 VMax = max(EGM);
 
 %% Extract A, V, T and baseline values
 %TODO : MAKE A FUNCTION TO EXTRACT A, V, T AND BASELINE VALUES%
 %Bounds are obtained manually from the plots
 %All values are normalized to the VMax
-ABounds = [134, 175];
-BBounds = [1,134];
+BBounds = [1450,1524];
+ABounds = [1524,1589];
 Baseline = EGM(BBounds(1):BBounds(2))./VMax;
 AWave = EGM(ABounds(1):ABounds(2))./VMax;%Extract the atrial signal
 ALength = size(AWave,1);%Width of the atrial signal
 
-VBounds = [251, 331];
+VBounds = [1652,1764];
 VWave = EGM(VBounds(1):VBounds(2))./VMax;%Extract the ventricular signal
 VLength = size(VWave,1);%Width of the ventricular signal
 
-TBounds = [443, 621];
+TBounds = [1800,2038];
 TWave = EGM(TBounds(1):TBounds(2))./VMax;%Extract the T Wave signal
 TLength = size(TWave,1);%Width of the T wave
 
@@ -43,10 +45,10 @@ TLength = size(TWave,1);%Width of the T wave
 
 %% Save Extracted data
 
-saveFile('AWave', AWave, fileNumber+channel-1, parentFile, num2str(HR), num2str(VMax), ALength);
-saveFile('VWave', VWave, fileNumber+channel-1, parentFile, num2str(HR), num2str(VMax), VLength);
-saveFile('TWave', TWave, fileNumber+channel-1, parentFile, num2str(HR), num2str(VMax), TLength);
-saveFile('Baseline', Baseline, fileNumber+channel-1, parentFile, num2str(HR), num2str(VMax), 999);
+saveFile('AWave', AWave, currentDirLength + channel - (startChannel-1), parentFile, num2str(HR), num2str(VMax), ALength);
+saveFile('VWave', VWave, currentDirLength + channel - (startChannel-1) , parentFile, num2str(HR), num2str(VMax), VLength);
+saveFile('TWave', TWave, currentDirLength + channel - (startChannel-1), parentFile, num2str(HR), num2str(VMax), TLength);
+saveFile('Baseline', Baseline, currentDirLength + channel - (startChannel-1), parentFile, num2str(HR), num2str(VMax), 999);
 
 
 %% Add Updated Data to file
@@ -60,11 +62,10 @@ if (exist(fileInfoName, 'file') == 2)
 elseif (exist(fileInfoName, 'file') == 0)
     fID = fopen(fileInfoName,'w');
     if fID~=-1
-    fprintf(fID, 'Filename,Channel,BaselineL,BaselineH,AWaveL,AWaveH,VWaveL,VWaveH,TWaveL,TwaveH,HR\n');
+    fprintf(fID, 'Filename,Channel,BaselineL,BaselineH,AWaveL,AWaveH,VWaveL,VWaveH,TWaveL,TWaveH,HR\n');
     fprintf(fID,'%s,%s\n', parentFile, join(string([channel BBounds ABounds VBounds TBounds HR]),','));
     end
     fclose(fID);
 end
 end
-
-
+disp('Done!')
--- signalGenScript.m
+++ signalGenScript.m
@@ -9,26 +9,36 @@ VWaveStruct = xWaveStruct(strcat('./Templates/VChannel/VWave/VWave',randChannels
 TWaveStruct = xWaveStruct(strcat('./Templates/VChannel/TWave/TWave',randChannels(3),'.csv'));
 BaselineStruct = xWaveStruct(strcat('./Templates/VChannel/Baseline/Baseline',randChannels(4),'.csv'));
 
-HR = 87;
+HR = 60;
 % -1 for the delays asks the script to choose a random sinus delay
 [simSignal, AVD,VTD,TAD] = simulateSignal(min(87, HR),AWaveStruct,VWaveStruct,TWaveStruct,BaselineStruct,-1,-1,-1);
 
 %%
-newHR = 320;
-change = newHR - HR;
+newHR = 20;
+% Calculate change in HR in ms
+change =  ceil(60e3./HR)- ceil(60e3./newHR);
 
-% Calculate new delays
+% Calculate new delays in m
 VTDchange = (-1)^(change>0)*ceil(sqrt(abs(change)));
-changeRemainder = (-1)^(change>0)*(abs(change) - abs(VTD_Change));
-newVTD = VTD + VTD_Change;
+changeRemainder = (-1)^(change>0)*(abs(change) - abs(VTDchange));
+newVTD = VTD + VTDchange;
 newTAD = TAD + ceil(changeRemainder/2);
 newAVD = AVD + floor(changeRemainder/2);
 
 % Create new signal with same components with modified delays
 [newSimSignal, AVD1,VTD1,TAD1] = simulateSignal(newHR,AWaveStruct,VWaveStruct,TWaveStruct,BaselineStruct,newAVD,newVTD,newTAD);
 
-%%
-figure(1)
-plot([repmat(simSignal,4,1); repmat(newSimSignal,4,1)])
+%% Ensure the HRs are as inputted
+a = repmat(simSignal,4,1);
+A = find(a==max(a));
+
+b = repmat(newSimSignal,4,1);
+B = find(b==max(b));
+
+sprintf('HR: %2.0f, newHR: %2.0f',60e3./(A(2)-A(1)),60e3/(B(2)-B(1)))
+%% Plot
+% figure(1)
+clf
+plot([repmat(simSignal,4,1);repmat(newSimSignal,4,1)])
 
 
